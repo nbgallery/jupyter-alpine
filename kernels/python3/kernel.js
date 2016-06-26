9cc5393d3ef3e714009553207576941e3af6d61a
Kyle King <kjking3@gmail.com>
2016-06-26 20:48:30 +0000

    added dynamic python 3 kernel

---
+++ kernels/python3/kernel.js
@@ -0,0 +1,52 @@
+define([
+  'base/js/namespace', 
+  'base/js/dialog', 
+  'services/config',
+  'base/js/utils'
+], function(
+  Jupyter, 
+  dialog, 
+  config,
+  utils
+) {
+  "use strict";    
+
+  var kernels = new config.ConfigSection('kernels', {
+    base_url: utils.get_body_data("baseUrl")
+  }); 
+
+  kernels.loaded.then(function() {
+    if (kernels.data['python3'] != 'installed') {
+      dialog.modal({
+        sanitize: false,
+        title: 'Installing Python 3 ...',
+        body: '<br>\
+               <div class="progress"> \
+                 <div class="progress-bar progress-bar-striped active" role="progressbar" style="width: 100%"> \
+                 </div> \
+               </div>'
+      });
+
+      $([Jupyter.events]).on('kernel_ready.Kernel', function() {
+        $('.modal').modal('hide');
+        kernels.update({python3: 'installed'});
+      });
+    }
+  });
+
+  kernels.load();
+
+  // original Ruby kernel.js
+  var onload = function() {
+    Jupyter.CodeCell.options_default['cm_config']['indentUnit'] = 2;
+    var cells = Jupyter.notebook.get_cells();
+    for (var i in cells) {
+      var c = cells[i];
+      if (c.cell_type === 'code') {
+        c.code_mirror.setOption('indentUnit', 2);
+      }
+    }
+  }
+  
+  return {onload:onload};
+});
