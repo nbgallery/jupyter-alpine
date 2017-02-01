fb59f2e0bd01d75c90ea3cc0089c24fc5db86e8e
Jeff <jeffyjefflabs@users.noreply.github.com>
2017-02-01 11:17:54 -0700

    Set $NBGALLERY_URL to override gallery location

--- config/jupyter/jupyter_notebook_config.py
+++ config/jupyter/jupyter_notebook_config.py
@@ -1,3 +1,5 @@
+import json
+import os
 import sys
 sys.path.append('/root/.jupyter/extensions/')
 
@@ -14,3 +16,13 @@ c.JupyterApp.allow_origin = 'https://nb.gallery'
 
 # needed to receive notebooks from the gallery
 c.JupyterApp.disable_check_xsrf = True
+
+# Override gallery location
+nbgallery_url = os.getenv('NBGALLERY_URL')
+if nbgallery_url:
+  print('Setting nbgallery url to %s' % nbgallery_url)
+  c.JupyterApp.allow_origin = nbgallery_url
+  config = json.loads(open('/root/.jupyter/nbconfig/common.json').read())
+  config['nbgallery']['url'] = nbgallery_url
+  with open('/root/.jupyter/nbconfig/common.json', 'w') as output:
+    output.write(json.dumps(config, indent=2))
