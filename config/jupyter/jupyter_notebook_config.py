380897e0caf90d31cc48b7e89118e378b47ec8bb
EchoWhisky <17090642+echowhisky@users.noreply.github.com>
2019-03-16 10:06:46 +0000

    making references to home directory more dynamic
    See issue #20 in the original repo

--- config/jupyter/jupyter_notebook_config.py
+++ config/jupyter/jupyter_notebook_config.py
@@ -1,7 +1,14 @@
 import json
 import os
 import sys
-sys.path.append('/root/.jupyter/extensions/')
+
+home = os.environ['HOME']
+jupyterPrefs_dir = '{0}/.jupyter/'.format(home)
+jP_static = jupyterPrefs_dir + 'static'
+jP_extensions = jupyterPrefs_dir + 'extensions/'
+jP_nbconfig_json = jupyterPrefs_dir + 'nbconfig/common.json'
+
+sys.path.append(jupyterPrefs_dir)
 
 c.JupyterApp.ip = '*'
 c.JupyterApp.port = 80
@@ -9,8 +16,8 @@ c.JupyterApp.open_browser = False
 c.JupyterApp.allow_credentials = True
 c.JupyterApp.nbserver_extensions = ['jupyter_nbgallery.status', 'jupyter_nbgallery.post']
 c.JupyterApp.reraise_server_extension_failures = True
-c.JupyterApp.extra_static_paths = ['/root/.jupyter/static']
-c.JupyterApp.extra_nbextensions_path = ['/root/.jupyter/extensions/']
+c.JupyterApp.extra_static_paths = [jP_static]
+c.JupyterApp.extra_nbextensions_path = [jP_extensions]
 c.JupyterApp.tornado_settings = {'static_url_prefix': '/Jupyter/static/'}
 c.JupyterApp.allow_origin = 'https://localhost:3000'
 
@@ -23,10 +30,10 @@ for var in [x for x in os.environ if x.startswith(config_prefix)]:
   c.JupyterApp[var[len(config_prefix):].lower()] = os.environ[var]
 
 def load_config():
-  return json.loads(open('/root/.jupyter/nbconfig/common.json').read())
+  return json.loads(open(jP_nbconfig_json).read())
 
 def save_config(config):
-  with open('/root/.jupyter/nbconfig/common.json', 'w') as output:
+  with open(jP_nbconfig_json) as output:
     output.write(json.dumps(config, indent=2))
 
 # Override gallery location
